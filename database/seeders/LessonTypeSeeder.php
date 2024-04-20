261c6e53f1bb69754cb7823b591ec174df909601
rheannemcintosh <rheannemcintosh@gmail.com>
2024-04-20 17:39:56 +0100

    Update the lesson type seeder to include the types of lessons
    This is an initial seeder to display the potential types of lessons that could be utilised throughout the platform.
    
    [GG-15]

--- database/seeders/LessonTypeSeeder.php
+++ database/seeders/LessonTypeSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class LessonTypeSeeder extends Seeder
 {
@@ -12,6 +13,32 @@ class LessonTypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('lesson_types')->insert([
+            'name' => 'Reading',
+        ]);
+
+        DB::table('lesson_types')->insert([
+            'name' => 'Map',
+        ]);
+
+        DB::table('lesson_types')->insert([
+            'name' => 'Picture',
+        ]);
+
+        DB::table('lesson_types')->insert([
+            'name' => 'Translation',
+        ]);
+
+        DB::table('lesson_types')->insert([
+            'name' => 'Matching',
+        ]);
+
+        DB::table('lesson_types')->insert([
+            'name' => 'Bingo',
+        ]);
+
+        DB::table('lesson_types')->insert([
+            'name' => 'Story',
+        ]);
     }
 }
