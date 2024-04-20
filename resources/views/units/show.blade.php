a8705123fa1716312240fd59ad3c5d0f5200080a
rheannemcintosh <rheannemcintosh@gmail.com>
2024-04-20 13:48:53 +0100

    Create a show unit page
    [GG-14]

---
+++ resources/views/units/show.blade.php
@@ -0,0 +1,10 @@
+<x-admin-layout>
+    <!-- Page Content -->
+    <div class="page-content">
+        <div class="unit">
+            <x-unit-heading :unit="$unit" />
+
+            <x-progress-bar />
+        </div>
+    </div>
+</x-admin-layout>
