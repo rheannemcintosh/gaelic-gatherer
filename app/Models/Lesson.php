7ed62c333aab74ac5512413bac548e260a7c2a09
rheannemcintosh <rheannemcintosh@gmail.com>
2024-04-21 15:13:54 +0100

    Add a relationship between units and lessons
    [GG-16]

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Lesson extends Model
 {
     use HasFactory;
+
+    /**
+     * The unit the lesson belongs to.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function unit(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo('App\Models\Unit');
+    }
 }
