774bd539cd6277fb9c35818f6010d36be869fe20
rheannemcintosh <rheannemcintosh@gmail.com>
2024-04-22 00:23:40 +0100

    Create a lesson seeder to initially populate the lessons table
    [GG-16]

--- database/seeders/LessonSeeder.php
+++ database/seeders/LessonSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class LessonSeeder extends Seeder
 {
@@ -12,6 +13,40 @@ class LessonSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('lessons')->insert([
+            'unit_id' => 1,
+            'lesson_type_id' => 1,
+            'name' => 'Scottish Greetings Overview',
+            'required' => true,
+        ]);
+
+        DB::table('lessons')->insert([
+            'unit_id' => 2,
+            'lesson_type_id' => 1,
+            'name' => 'Scottish Places Overview',
+            'description' => 'Learn how to greet people in Scottish Gaelic.',
+            'required' => true,
+        ]);
+
+        DB::table('lessons')->insert([
+            'unit_id' => 3,
+            'lesson_type_id' => 1,
+            'name' => 'Scottish Food & Drink Overview',
+            'required' => true,
+        ]);
+
+        DB::table('lessons')->insert([
+            'unit_id' => 4,
+            'lesson_type_id' => 1,
+            'name' => 'Scottish Weather Overview',
+            'required' => true,
+        ]);
+
+        DB::table('lessons')->insert([
+            'unit_id' => 5,
+            'lesson_type_id' => 1,
+            'name' => 'Scottish Gaelic Alphabet Overview',
+            'required' => true,
+        ]);
     }
 }
