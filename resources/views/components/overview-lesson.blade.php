233294891a5d9e3cda9fa1fec717d7c8e67fa772
rheannemcintosh <rheannemcintosh@gmail.com>
2024-04-27 17:41:07 +0100

    Display the weather overview lesson
    Show an introduction to the weather lesson, as well as how to say "It is..." and nine different types of weather.
    
    [GG-20]

--- resources/views/components/overview-lesson.blade.php
+++ resources/views/components/overview-lesson.blade.php
@@ -1,5 +1,7 @@
 <div class="p-8">
     @if($lesson->name == 'Numbers Overview')
         <x-numbers-overview />
+    @elseif ($lesson->name == 'Weather Overview')
+        <x-weather-overview />
     @endif
 </div>
--- resources/views/components/weather-overview.blade.php
+++ resources/views/components/weather-overview.blade.php
@@ -1,3 +1,59 @@
 <div>
-    <!-- If you do not have a consistent goal in life, you can not live it in a consistent way. - Marcus Aurelius -->
+    <p class="mb-2 text-center">
+        Welcome to our Scottish Gaelic overview lesson on the weather. Scottish people love to talk about the weather, and it's a great way to start a conversation. So let's delve into the world of weather in Scottish Gaelic and learn how to say what the weather is like!
+    </p>
+    <div class="flex justify-center">
+        <div class="overflow-x-auto">
+            <table class="table-auto">
+                <thead>
+                <tr>
+                    <th class="px-4 py-2 text-center text-xl">English</th>
+                    <th class="px-4 py-2 text-center text-xl">Gaelic</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="border px-4 py-2 text-center">It is ...</td>
+                        <td class="border px-4 py-2 text-center font-bold">Tha i ...</td>
+                    </tr>
+                    <tr>
+                        <td class="border px-4 py-2 text-center">Hot</td>
+                        <td class="border px-4 py-2 text-center font-bold">Teth</td>
+                    </tr>
+                    <tr>
+                        <td class="border px-4 py-2 text-center">Warm</td>
+                        <td class="border px-4 py-2 text-center font-bold">Blàth</td>
+                    </tr>
+                    <tr>
+                        <td class="border px-4 py-2 text-center">Cold</td>
+                        <td class="border px-4 py-2 text-center font-bold">Fuar</td>
+                    </tr>
+                    <tr>
+                        <td class="border px-4 py-2 text-center">Freezing</td>
+                        <td class="border px-4 py-2 text-center font-bold">Reòiteach</td>
+                    </tr>
+                    <tr>
+                        <td class="border px-4 py-2 text-center">Sunny</td>
+                        <td class="border px-4 py-2 text-center font-bold">Grianach</td>
+                    </tr>
+                    <tr>
+                        <td class="border px-4 py-2 text-center">Windy</td>
+                        <td class="border px-4 py-2 text-center font-bold">Gaothach</td>
+                    </tr>
+                    <tr>
+                        <td class="border px-4 py-2 text-center">Rainy</td>
+                        <td class="border px-4 py-2 text-center font-bold">Fliuch</td>
+                    </tr>
+                    <tr>
+                        <td class="border px-4 py-2 text-center">Foggy</td>
+                        <td class="border px-4 py-2 text-center font-bold">Ceòthach</td>
+                    </tr>
+                    <tr>
+                        <td class="border px-4 py-2 text-center">Snowy</td>
+                        <td class="border px-4 py-2 text-center font-bold">Sneachda</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
