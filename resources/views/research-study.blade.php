bb6bb8b4e29efeb8d0742fd4b39997e044c2fa87
rheannemcintosh <rheannemcintosh@gmail.com>
2024-04-28 00:09:31 +0100

    Update the research study text with the up to date information
    [GG-10]

--- resources/views/research-study.blade.php
+++ resources/views/research-study.blade.php
@@ -1,24 +1,30 @@
-<h1>Thank you for your interest in participating in our Study!</h1>
-<p>Welcome to the Gaelic Gatherer Study! We are thrilled to invite you to participate in this research endeavor aimed at evaluating the effectiveness of gamified badges on knowledge retention among users.</p>
-<h2>Aim of the Study</h2>
-<p>Our primary goal is to assess how gamified badges influence the retention of knowledge acquired through the Gaelic Gatherer platform. By participating in this study, you will contribute valuable insights to our understanding of how gamification can enhance language learning outcomes.</p>
-<h2>About Gaelic Gatherer</h2>
-<p>The Gaelic Gatherer application is a platform designed to introduce users to the basics of Scottish Gaelic in an engaging and interactive manner. Through a series of lessons and activities, participants will have the opportunity to explore the rich linguistic heritage of Scotland.</p>
-<h2>What Does the Study Involve?</h2>
-<p>Upon signing up, participants will be invited to utilize the Gaelic Gatherer platform, where they can engage with various learning materials at their own pace. The study requires participants to read information on five key topics related to Scottish Gaelic, which should take approximately five minutes.</p>
-<p>Following the exploration of the platform, participants will be asked to provide some personal information, including age, gender identity, nationality, residence, and connections to Scotland and Gaelic.</p>
-<p>Subsequently, participants will be prompted to take a short test on Scottish Gaelic, consisting of ten questions, which should take around ten minutes to complete. Additionally, after one hour, participants will receive an email encouraging them to optionally complete another brief test on Gaelic. This test will be repeated after two weeks to gauge long-term retention.</p>
-<h2>Personal Data</h2>
-<p>Along with data stored in relation to the use of the application, the following personal data is collected to be used in the study:</p>
-<ul>
-    <li>Age</li>
-    <li>Gender</li>
-    <li>Nationality</li>
-    <li>Residence</li>
-    <li>Connections to Scotland</li>
-    <li>Knowledge of Gaelic</li>
-</ul>
-<p>Participants are assured that all personal data will be stored securely in compliance with data protection regulations. The data collected will be used solely for research purposes, will be anonymised for the purpose of the study and will only be shared with relevant University of Bath professionals.</p>
-<h1>Still Interested?</h1>
-<p>If you are still interested, and are over the age of 18 years old, register below!</p>
-<a href="" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Register Now</a>
+<x-app-layout>
+
+    <h2 class="text-center font-bold mb-2">Thank you for your interest in participating in this study!</h2>
+    <p class="mb-4 text-center">Welcome to the Gaelic Gatherer Study! I am thrilled to invite you to participate in this research endeavour, which aims to evaluate the effectiveness of gamified badges in promoting knowledge retention.</p>
+
+    <h2 class="text-center font-bold mb-2">Aim of the Study</h2>
+    <p class="mb-4 text-center">The primary goal of this research is to assess how gamified badges influence the retention of knowledge acquired through the Gaelic Gatherer platform. By participating in this study, you will contribute valuable insights to understanding how gamification can enhance the knowledge retention of language learning.</p>
+
+    <h2 class="text-center font-bold mb-2">What Does the Study Involve?</h2>
+    <p>Participants will explore an application called Gaelic Gatherer at their own pace, engaging with learning materials on key topics in Scottish Gaelic. They'll then provide some personal information before completing a small quiz on Scottish Gaelic, followed by quizzes one hour and two weeks later to measure knowledge retention.</p>
+
+    <h2 class="text-center font-bold mb-2">Who Can Participate?</h2>
+    <p class="mb-4 text-center">Individuals over 18 years old, residing in the UK, and without significant prior knowledge of Scottish Gaelic are eligible to participate.</p>
+
+    <h2 class="text-center font-bold mb-2">What is the Withdrawal Process?</h2>
+    <p class="mb-4 text-center">Participants can withdraw from this project by discontinuing platform use, selecting withdrawal options in emails or on the platform, or contacting the researcher via email.</p>
+
+    <h2 class="text-center font-bold mb-2">What personal data will be stored?</h2>
+    <p class="mb-4 text-center">Participants' personal information, questionnaire responses, quiz performance, and platform usage are collected to enrich our understanding of Scottish Gaelic language learning.</p>
+    <p class="mb-4 text-center">Participants are assured that all personal data will be stored securely in compliance with data protection regulations. The data collected will be used solely for research purposes, anonymised for the purpose of the study, and shared only with relevant University of Bath professionals.</p>
+
+    <h2 class="text-center font-bold mb-2">Still Interested?</h2>
+    <p class="mb-4 text-center">If you are still interested and meet the criteria, please register below!</p>
+
+    <div class="flex justify-center">
+        <a href="/research-study/download-pdf" class="mr-2 inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150">Download Participant Information Sheet</a>
+        <a href="/register" class="ml-2 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Register for the Study</a>
+    </div>
+
+</x-app-layout>
