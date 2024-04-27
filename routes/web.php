657bec4915cdf3804d541ec7858ea62dec96ad3e
rheannemcintosh <rheannemcintosh@gmail.com>
2024-04-28 00:10:03 +0100

    Add a method to download a full participant information sheet
    [GG-10]

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\LessonController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UnitController;
@@ -35,6 +36,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/units/{slug}', [UnitController::class, 'show'])->name('units.show');
     Route::get('/units/{slug}/lessons/{lesson}', [LessonController::class, 'show'])->name('lessons.show');
 });
+
+Route::get('/research-study/download-pdf', [RegisteredUserController::class, 'downloadParticipantInformationSheetPDF'])->name('research-study.download-pdf');
 
 require __DIR__.'/auth.php';
 
