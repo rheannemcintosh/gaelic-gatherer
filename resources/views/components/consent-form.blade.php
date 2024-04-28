49a61f733ca66b5fccf5db9503b009d2f5c3090c
rheannemcintosh <rheannemcintosh@gmail.com>
2024-04-28 14:22:03 +0100

    Update the name of the consent field on the consent form
    [GG-12]

--- resources/views/components/consent-form.blade.php
+++ resources/views/components/consent-form.blade.php
@@ -3,8 +3,8 @@
     <div class="p-2">
         @foreach (\App\Helpers\ConsentHelper::CONSENT_STATEMENTS as $statement)
             <div class="flex items-center mb-1">
-                <input type="checkbox" id="statement[{{ $loop->index }}]" name="statement[{{ $loop->index }}]" class="mr-4">
-                <label for="statement[{{ $loop->index }}]" class="text-sm">{{ $statement }}</label>
+                <input type="checkbox" id="initial_consent_statement[{{ $loop->index }}]" name="initial_consent_statement[{{ $loop->index }}]" class="mr-4">
+                <label for="initial_consent_statement[{{ $loop->index }}]" class="text-sm">{{ $statement }}</label>
             </div>
         @endforeach
     </div>
