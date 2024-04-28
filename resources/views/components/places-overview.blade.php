59efef3d58575872ef3582935657282a00801b54
rheannemcintosh <rheannemcintosh@gmail.com>
2024-04-28 22:57:52 +0100

    Display the places overview lesson
    Show an introduction to the places lesson, as well as how to ask where someone is from, and how to tell people where you are from.
    
    [GG-18]

--- resources/views/components/places-overview.blade.php
+++ resources/views/components/places-overview.blade.php
@@ -1,3 +1,59 @@
 <div>
-    <!-- Life is available only in the present moment. - Thich Nhat Hanh -->
+    <p class="mb-2 text-center">
+        Welcome to our Scottish Gaelic overview lesson on places. Many places in Scotland have Gaelic Origins, so let's delve into the world of places in Scottish Gaelic and learn how to say where we're from!
+    </p>
+    <div class="flex justify-center">
+        <div class="overflow-x-auto">
+            <table class="table-auto">
+                <thead>
+                <tr>
+                    <th class="px-4 py-2 text-center text-xl">English</th>
+                    <th class="px-4 py-2 text-center text-xl">Gaelic</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="border px-4 py-2 text-center">Where are you from?</td>
+                        <td class="border px-4 py-2 text-center font-bold">Cò às a tha thu?</td>
+                    </tr>
+                    <tr>
+                        <td class="border px-4 py-2 text-center">I am from ...</td>
+                        <td class="border px-4 py-2 text-center font-bold">Tha mi à ...</td>
+                    </tr>
+                    <tr>
+                        <td class="border px-4 py-2 text-center">Scotland</td>
+                        <td class="border px-4 py-2 text-center font-bold">Alba</td>
+                    </tr>
+                    <tr>
+                        <td class="border px-4 py-2 text-center">Aberdeen</td>
+                        <td class="border px-4 py-2 text-center font-bold">Obar Dheathain</td>
+                    </tr>
+                    <tr>
+                        <td class="border px-4 py-2 text-center">Edinburgh</td>
+                        <td class="border px-4 py-2 text-center font-bold">Dùn Èideann</td>
+                    </tr>
+                    <tr>
+                        <td class="border px-4 py-2 text-center">Glasgow</td>
+                        <td class="border px-4 py-2 text-center font-bold">Glaschu</td>
+                    </tr>
+                    <tr>
+                        <td class="border px-4 py-2 text-center">Inverness</td>
+                        <td class="border px-4 py-2 text-center font-bold">Inbhir Nis</td>
+                    </tr>
+                    <tr>
+                        <td class="border px-4 py-2 text-center">England</td>
+                        <td class="border px-4 py-2 text-center font-bold">Sasainn</td>
+                    </tr>
+                    <tr>
+                        <td class="border px-4 py-2 text-center">Wales</td>
+                        <td class="border px-4 py-2 text-center font-bold">A' Chuimrigh</td>
+                    </tr>
+                    <tr>
+                        <td class="border px-4 py-2 text-center">Northern Ireland</td>
+                        <td class="border px-4 py-2 text-center font-bold">Èirinn a Tuath</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
