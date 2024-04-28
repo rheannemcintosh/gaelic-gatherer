814259cd96a9182105f69a3adf790dbdd3c168c2
rheannemcintosh <rheannemcintosh@gmail.com>
2024-04-28 13:32:25 +0100

    Update the consent form with new consent statements
    [GG-12]

--- app/Helpers/ConsentHelper.php
+++ app/Helpers/ConsentHelper.php
@@ -6,12 +6,11 @@ class ConsentHelper
 {
     public const CONSENT_STATEMENTS = [
         'I have been provided with information explaining what participation in this project involves.',
-        'I have had an opportunity to ask questions and discuss this project.',
-        'I have received satisfactory answers to all questions I have asked.',
+        'I know how to contact the researcher(s) if I have any questions or concerns.',
+        'I have received satisfactory answers to any questions I have asked.',
         'I have received enough information about the project to make a decision about my participation.',
         'I understand that I am free to withdraw my consent to participate in the project at any time without having to give a reason for withdrawing.',
-        'I understand that I am free to withdraw my data within two weeks of my participation.',
-        'I understand the nature and purpose of the procedures involved in this project. These have been communicated to me on the information sheet accompanying this form.',
+        'I understand the nature and purpose of the procedures involved in this project. These have been communicated to me on the information sheet on the previous page.',
         'I understand and acknowledge that the investigation is designed to promote scientific knowledge and that the University of Bath will use the data I provide only for the purpose(s) set out in the information sheet.',
         'I understand the data I provide will be treated as confidential, and that on completion of the project my name or other identifying information will not be disclosed in any presentation or publication of the research.',
         'I understand that my consent to use the data I provide is conditional upon the University complying with its duties and obligations under the Data Protection Act.',
