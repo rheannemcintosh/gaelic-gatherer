fa647c4ce3b6c229ce27544b765cb20388e418fa
rheannemcintosh <rheannemcintosh@gmail.com>
2024-05-05 00:19:57 +0100

    Show the post study questionnaire page
    [GG-24]

--- app/Http/Controllers/PostStudyQuestionnaireController.php
+++ app/Http/Controllers/PostStudyQuestionnaireController.php
@@ -4,4 +4,11 @@ namespace App\Http\Controllers;
 
 class PostStudyQuestionnaireController extends Controller
 {
+    /**
+     * Display the specified resource.
+     */
+    public function show()
+    {
+        return view('pages.post-study-questionnaire');
+    }
 }
---
+++ resources/views/pages/post-study-questionnaire.blade.php
@@ -0,0 +1,7 @@
+<x-admin-layout :hideButton="true">
+    <h1 class="text-lg font-bold">Post Study Questionnaire</h1>
+    <p>
+        Thank you for exploring the Gaelic Gatherer platform! To better understand your post-study motivation and your connections to Scotland, and Scottish Gaelic, please take a moment to complete this brief questionnaire.
+    </p>
+    <x-post-study-questionnaire />
+</x-admin-layout>
