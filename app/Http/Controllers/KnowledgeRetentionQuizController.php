12f06d640fbe7c3f1529dbf764f9f0146cbf647e
rheannemcintosh <rheannemcintosh@gmail.com>
2024-05-06 00:37:00 +0100

    Store the results of the knowledge retention quiz
    [GG-25]

--- app/Http/Controllers/KnowledgeRetentionQuizController.php
+++ app/Http/Controllers/KnowledgeRetentionQuizController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\KnowledgeRetentionHelper;
+use App\Models\KnowledgeRetentionQuizResult;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class KnowledgeRetentionQuizController extends Controller
 {
@@ -27,7 +31,41 @@ class KnowledgeRetentionQuizController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        // Validate the incoming request
+        $validatedData = $request->validate([
+            'knowledge_retention_quiz.*' => 'required', // Assuming all quiz answers are required
+        ]);
+
+        $score = 0;
+
+        for ($i = 1; $i <= 10; $i++) {
+            $question = $validatedData['knowledge_retention_quiz'][$i];
+            $correctAnswer = KnowledgeRetentionHelper::QUESTIONS[$i]['correct_answer'];
+            $isCorrect = $question === $correctAnswer;
+
+            if ($isCorrect) {
+                $score++;
+            }
+        }
+
+        KnowledgeRetentionQuizResult::create([
+            'user_id' => Auth::id(),
+            'quiz' => 1,
+            'question_1' => $validatedData['knowledge_retention_quiz'][1],
+            'question_2' => $validatedData['knowledge_retention_quiz'][2],
+            'question_3' => $validatedData['knowledge_retention_quiz'][3],
+            'question_4' => $validatedData['knowledge_retention_quiz'][4],
+            'question_5' => $validatedData['knowledge_retention_quiz'][5],
+            'question_6' => $validatedData['knowledge_retention_quiz'][6],
+            'question_7' => $validatedData['knowledge_retention_quiz'][7],
+            'question_8' => $validatedData['knowledge_retention_quiz'][8],
+            'question_9' => $validatedData['knowledge_retention_quiz'][9],
+            'question_10' => $validatedData['knowledge_retention_quiz'][10],
+            'score' => $score
+        ]);
+
+        return redirect(RouteServiceProvider::HOME);
     }
 
     /**
