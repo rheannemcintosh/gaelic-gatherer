04ae44712c9d6324e74992885d533231945130b7
rheannemcintosh <rheannemcintosh@gmail.com>
2024-05-12 23:59:00 +0100

    Create the lesson user table
    Use laravel's make:migration command to create a pivot table to link a lesson with a user.
    
    [GG-22]

---
+++ database/migrations/2024_05_12_223233_create_lesson_user_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lesson_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('lesson_id')->constrained();
+            $table->boolean('completed')->default(false);
+            $table->timestamp('completed_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lesson_user');
+    }
+};
