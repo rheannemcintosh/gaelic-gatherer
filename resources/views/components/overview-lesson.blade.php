31d8a960191c7e70c7166a2ab944fa67f0026f5d
rheannemcintosh <rheannemcintosh@gmail.com>
2024-05-13 13:30:15 +0100

    Add a form to allow a user to complete a lesson
    [GG-22]

--- resources/views/components/overview-lesson.blade.php
+++ resources/views/components/overview-lesson.blade.php
@@ -1,4 +1,10 @@
 <div class="p-8">
+    <form method="POST" action="{{ route('user-lessons.store', ['lesson' => $lesson->id]) }}">
+
+        <!-- CSRF Token -->
+        @csrf
+
+        <!-- Display Lesson -->
         @switch($lesson->name)
             @case('Numbers Overview')
                 <x-numbers-overview />
@@ -16,4 +22,17 @@
                 <x-greetings-overview />
                 @break
         @endswitch
+
+        <!-- Complete Lesson Button -->
+        <div class="mt-8">
+            <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+                <div class="w-full flex items-center justify-center">
+                    <button class="justify-center bg-gradient-to-r from-blue-700 to-blue-500 px-8 py-4 rounded-lg shadow-md transition-all duration-200 ease-in-out transform hover:scale-105">
+                        <span class="text-white font-bold">Complete Lesson</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+    </form>
 </div>
