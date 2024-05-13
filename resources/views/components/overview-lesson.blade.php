6eeee5ebbbb9347ddfd996d7669ef1f6f8e19eca
rheannemcintosh <rheannemcintosh@gmail.com>
2024-05-13 13:07:51 +0100

    Update the overview lesson blade to use a switch statement
    [GG-22]

--- resources/views/components/overview-lesson.blade.php
+++ resources/views/components/overview-lesson.blade.php
@@ -1,13 +1,19 @@
 <div class="p-8">
-    @if($lesson->name == 'Numbers Overview')
-        <x-numbers-overview />
-    @elseif ($lesson->name == 'Weather Overview')
-        <x-weather-overview />
-    @elseif ($lesson->name == 'Food & Drink Overview')
-        <x-food-and-drink-overview />
-    @elseif ($lesson->name == 'Places Overview')
-        <x-places-overview />
-    @elseif ($lesson->name == 'Greetings Overview')
-        <x-greetings-overview />
-    @endif
+        @switch($lesson->name)
+            @case('Numbers Overview')
+                <x-numbers-overview />
+                @break
+            @case('Weather Overview')
+                <x-weather-overview />
+                @break
+            @case('Food & Drink Overview')
+                <x-food-and-drink-overview />
+                @break
+            @case('Places Overview')
+                <x-places-overview />
+                @break
+            @case('Greetings Overview')
+                <x-greetings-overview />
+                @break
+        @endswitch
 </div>
