d4f9c735fd5c197d4bcc7301567898886ba0c8a6
rheannemcintosh <rheannemcintosh@gmail.com>
2024-05-14 21:07:50 +0100

    Create a badges table to store the badge name, description and icon
    [GG-26]

--- database/migrations/2024_05_11_102940_create_badges_table.php
+++ database/migrations/2024_05_11_102940_create_badges_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('badges', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->string('icon')->nullable();
             $table->timestamps();
         });
     }
