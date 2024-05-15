b3e61a0e097b7121158b5ae15304377bca5df2bc
rheannemcintosh <rheannemcintosh@gmail.com>
2024-05-15 23:17:05 +0100

    Add a temporary route to attach a badge to a user
    [GG-27]

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,12 @@
 
     <!-- Page Content -->
     <div>
-        Example Content
+        <form method="POST" action="{{ route('badge-users.store', ['badge' => 2]) }}">
+            @csrf
+            <button class="justify-center bg-gradient-to-r from-blue-700 to-blue-500 px-8 py-4 rounded-lg shadow-md transition-all duration-200 ease-in-out transform hover:scale-105">
+                <span class="text-white font-bold">Add Badge</span>
+            </button>
+        </form>
     </div>
 
 </x-admin-layout>
