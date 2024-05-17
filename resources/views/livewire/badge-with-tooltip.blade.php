095a06da9ab87cbe96d247dea0d65aa86e50843c
rheannemcintosh <rheannemcintosh@gmail.com>
2024-05-17 14:27:26 +0100

    Display badges with a different colour if they have been completed

--- resources/views/livewire/badge-with-tooltip.blade.php
+++ resources/views/livewire/badge-with-tooltip.blade.php
@@ -1,8 +1,13 @@
 <div x-data="{ show: false }" class="relative">
     <div x-on:mouseover="show = true" x-on:mouseout="show = false">
         <!-- Badge -->
+
         <div class="flex justify-center pb-2">
-            <div class="w-16 h-16 bg-gray-500 rounded-full"></div>
+            @if ($badge->pivot->completed)
+                <div class="w-16 h-16 bg-red-500 rounded-full"></div>
+            @else
+                <div class="w-16 h-16 bg-gray-500 rounded-full"></div>
+            @endif
         </div>
 
         <!-- Badge Name -->
@@ -16,7 +21,7 @@
         x-show="show"
         x-on:mouseover="show = true"
         x-on:mouseout="show = false"
-        class="break-inside-avoid-column z-50 absolute inset-0 bg-blue-900 opacity-95 text-xs text-white px-8 py-2 rounded-xl flex justify-center items-center text-center"
+        class="break-inside-avoid-column z-50 absolute inset-0 bg-blue-900 opacity-95 text-xs text-white px-4 py-2 rounded-xl flex justify-center items-center text-center"
     >
         <span class="">{{ $badge->description }}</span>
     </div>
