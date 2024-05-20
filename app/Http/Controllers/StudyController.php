cd3be6c90e816a87047b8be707a35695eae3b7e9
rheannemcintosh <rheannemcintosh@gmail.com>
2024-05-21 00:15:21 +0100

    Show the welcome page
    [GG-68]

--- app/Http/Controllers/StudyController.php
+++ app/Http/Controllers/StudyController.php
@@ -4,4 +4,10 @@ namespace App\Http\Controllers;
 
 class StudyController extends Controller
 {
+    // Show the welcome page to research participants
+    public function showWelconePage()
+    {
+        return view('pages.welcome');
+    }
+
 }
