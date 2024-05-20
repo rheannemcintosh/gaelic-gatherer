3fda76b12f0f517a7d3dc3ef0075af31f5a87e04
rheannemcintosh <rheannemcintosh@gmail.com>
2024-05-21 00:15:38 +0100

    Update the relevant user data when starting the study
    [GG-68]

--- app/Http/Controllers/StudyController.php
+++ app/Http/Controllers/StudyController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserData;
+use Illuminate\Support\Facades\Auth;
+
 class StudyController extends Controller
 {
     // Show the welcome page to research participants
@@ -10,4 +13,26 @@ class StudyController extends Controller
         return view('pages.welcome');
     }
 
+    // Start the study
+    public function startTheStudy()
+    {
+        // Get the authenticated user
+        $user = Auth::user();
+
+        // Update the study consent field
+        $user->update([
+            'study_consent' => true
+        ]);
+
+        // Find the user data field
+        $userData = UserData::find($user->id);
+
+        // Update the study started at field
+        $userData->update([
+            'study_started_at' => now()
+        ]);
+
+        // Redirect to the overview page
+        return redirect()->route('units.index');
+    }
 }
