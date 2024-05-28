7a6e43b921f6f6f6ef5464901d3baf8757c108d3
rheannemcintosh <rheannemcintosh@gmail.com>
2024-05-28 22:07:58 +0100

    Create the livewire php file to handle the badges popup
    [GG-74]

---
+++ app/Livewire/BadgesPopup.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class BadgesPopup extends Component
+{
+    /**
+     * @var bool Whether to show the popup.
+     */
+    public $showPopup = false;
+
+    /**
+     * @var array The new badges to display.
+     */
+    public $newBadges = [];
+
+    /**
+     * Create a new component instance.
+     */
+    public function mount($newBadges)
+    {
+        $this->showPopup = true;
+
+        // Initialize with some badge data, or fetch from a database
+        $this->newBadges = $newBadges;
+    }
+
+    /**
+     * Toggle the popup visibility.
+     */
+    public function togglePopup()
+    {
+        $this->showPopup = !$this->showPopup;
+    }
+
+    /**
+     * Render the component.
+     */
+    public function render()
+    {
+        return view('livewire.badges-popup');
+    }
+}
