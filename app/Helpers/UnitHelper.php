fbe9d9c387c4503f81806f268cc646e4c07a4270
rheannemcintosh <rheannemcintosh@gmail.com>
2024-05-29 18:31:14 +0100

    Calculate the completition percentages of units
    [GG-73]

--- app/Helpers/UnitHelper.php
+++ app/Helpers/UnitHelper.php
@@ -2,6 +2,24 @@
 
 namespace App\Helpers;
 
+use App\Models\Lesson;
+
 class UnitHelper
 {
+    public static function getCompletitionPercentagesOfUnits($units)
+    {
+        $percentageComplete = [];
+
+        foreach ($units as $unit) {
+            $numLessonsInUnit = Lesson::where('unit_id', $unit->id)->count();
+
+            $completedLessonsInUnit = auth()->user()->completedLessons()->whereHas('unit', function ($query) use ($unit) {
+                $query->where('title', $unit->title);
+            })->count();
+
+            $percentageComplete[$unit->id] = ($completedLessonsInUnit / $numLessonsInUnit) * 100;
+        }
+
+        return $percentageComplete;
+    }
 }
