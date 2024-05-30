c709672399da751f18346b4ff77cba60b58e8140
rheannemcintosh <rheannemcintosh@gmail.com>
2024-05-30 22:18:46 +0100

    Create a lock circle component for the platform progress bar
    [GG-70]

---
+++ resources/views/components/circles/lock-circle.blade.php
@@ -0,0 +1,5 @@
+<button disabled class="disabled:cursor-not-allowed w-8 h-8 bg-white border-2 border-blue-600 rounded-full mb-2 flex justify-center items-center">
+    <span class="material-symbols-rounded text-sm text-blue-600">
+        lock
+    </span>
+</button>
