32f3d88d7628bf9a997d328f9f3dcc13247d95dd
rheannemcintosh <rheannemcintosh@gmail.com>
2024-05-31 00:15:33 +0100

    Refactor the unit controller
    [GG-83]

--- app/Http/Controllers/UnitController.php
+++ app/Http/Controllers/UnitController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Helpers\UnitHelper;
 use App\Models\Unit;
-use Illuminate\Http\Request;
 
 class UnitController extends Controller
 {
@@ -13,51 +12,9 @@ class UnitController extends Controller
      */
     public function index()
     {
-        $units = Unit::with('lessons')->get();
+        $units           = Unit::with('lessons')->get();
+        $unitPercentages = UnitHelper::getCompletionPercentagesOfAllUnits($units);
 
-        $percentageComplete = UnitHelper::getCompletitionPercentagesOfUnits($units);
-
-        return view('pages.overview', compact('units', 'percentageComplete'));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return view('pages.overview', compact('units', 'unitPercentages'));
     }
 }
