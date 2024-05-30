b4b8eb34d777bd6b0b8e8826e6196a301a42d29f
rheannemcintosh <rheannemcintosh@gmail.com>
2024-05-30 22:19:21 +0100

    Update the platform progress component
    [GG-70]

--- resources/views/components/platform-progress.blade.php
+++ resources/views/components/platform-progress.blade.php
@@ -1,60 +1,69 @@
 <h1 class="mt-8 flex justify-center items-center font-bold text-blue-950">Study Progress</h1>
 <div class="flex justify-center items-center ">
-
     <div class="flex space-x-8 p-4 text-xs">
         <div class="flex flex-col items-center">
             @if (auth()->user()->initial_consent)
-                <div class="w-8 h-8 bg-blue-500 rounded-full mb-2"></div>
-            @else
-                <div class="w-8 h-8 bg-white border-red-600 rounded-full mb-2"></div>
+                <x-check-circle />
             @endif
             <span class="w-20 text-center text-gray-700 break-words">Initial Registration</span>
         </div>
         <div class="flex flex-col items-center">
-            @if (auth()->user()->pre_study_consent)
-                <div class="w-8 h-8 bg-blue-500 rounded-full mb-2"></div>
+            @if (auth()->user()->pre_study_consent && isset(auth()->user()->data->pre_study_completed_at))
+                <x-check-circle />
+            @elseif (auth()->user()->initial_consent)
+                <x-target-circle :route="route('pre-study-questionnaire.show.consent')" />
             @else
-                <div class="w-8 h-8 bg-white border-2 border-blue-500 rounded-full mb-2"></div>
+                <x-lock-circle />
             @endif
             <span class="w-20 text-center text-gray-700 break-words">Pre-Study Questionnaire</span>
         </div>
         <div class="flex flex-col items-center">
-            @if (auth()->user()->study_consent)
-                <div class="w-8 h-8 bg-blue-500 rounded-full mb-2"></div>
+            @if (auth()->user()->study_consent && isset(auth()->user()->data->study_completed_at))
+                <x-check-circle />
+            @elseif (auth()->user()->pre_study_consent && isset(auth()->user()->data->pre_study_completed_at))
+                <x-target-circle :route="route('units.index')" />
             @else
-                <div class="w-8 h-8 bg-white border-2 border-blue-500 rounded-full mb-2"></div>
+                <x-lock-circle />
             @endif
             <span class="w-20 text-center text-gray-700 break-words">Explore the Platform</span>
         </div>
         <div class="flex flex-col items-center">
-            @if (auth()->user()->post_study_consent)
-                <div class="w-8 h-8 bg-blue-500 rounded-full mb-2"></div>
+            @if (auth()->user()->post_study_consent && isset(auth()->user()->data->post_study_completed_at))
+                <x-check-circle />
+            @elseif (auth()->user()->study_consent && isset(auth()->user()->data->study_completed_at))
+                <x-target-circle :route="route('post-study-questionnaire.show.consent')" />
             @else
-                <div class="w-8 h-8 bg-white border-2 border-blue-500 rounded-full mb-2"></div>
+                <x-lock-circle />
             @endif
             <span class="w-20 text-center text-gray-700 break-words">Post-Study Questionnaire</span>
         </div>
         <div class="flex flex-col items-center">
-            @if (auth()->user()->quiz_one_consent)
-                <div class="w-8 h-8 bg-blue-500 rounded-full mb-2"></div>
+            @if (auth()->user()->quiz_one_consent && isset(auth()->user()->data->quiz_one_completed_at))
+                <x-check-circle />
+            @elseif (auth()->user()->post_study_consent && isset(auth()->user()->data->post_study_completed_at))
+                <x-target-circle :route="route('knowledge-retention-quiz.show.consent', ['quiz' => 1])" />
             @else
-                <div class="w-8 h-8 bg-white border-2 border-blue-500 rounded-full mb-2"></div>
+                <x-lock-circle />
             @endif
             <span class="w-20 text-center text-gray-700 break-words">Quiz 1 (Immediate)</span>
         </div>
         <div class="flex flex-col items-center">
-            @if (auth()->user()->quiz_two_consent)
-                <div class="w-8 h-8 bg-blue-500 rounded-full mb-2"></div>
+            @if (auth()->user()->quiz_two_consent && isset(auth()->user()->data->quiz_two_completed_at))
+                <x-check-circle />
+            @elseif (auth()->user()->quiz_one_consent && isset(auth()->user()->data->quiz_one_completed_at))
+                <x-target-circle :route="route('knowledge-retention-quiz.show.consent', ['quiz' => 2])" />
             @else
-                <div class="w-8 h-8 bg-white border-2 border-blue-500 rounded-full mb-2"></div>
+                <x-lock-circle />
             @endif
             <span class="w-20 text-center text-gray-700 break-words">Quiz 2 (1Hour)</span>
         </div>
         <div class="flex flex-col items-center">
-            @if (auth()->user()->quiz_three_consent)
-                <div class="w-8 h-8 bg-blue-500 rounded-full mb-2"></div>
+            @if (auth()->user()->quiz_three_consent && isset(auth()->user()->data->quiz_three_completed_at))
+                <x-check-circle />
+            @elseif (auth()->user()->quiz_two_consent && isset(auth()->user()->data->quiz_two_completed_at))
+                <x-target-circle :route="route('knowledge-retention-quiz.show.consent', ['quiz' => 3])" />
             @else
-                <div class="w-8 h-8 bg-white border-2 border-blue-500 rounded-full mb-2"></div>
+                <x-lock-circle />
             @endif
             <span class="w-20 text-center text-gray-700 break-words">Quiz 3 (14Days)</span>
         </div>
