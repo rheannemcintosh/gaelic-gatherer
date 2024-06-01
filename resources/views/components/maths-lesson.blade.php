7ea3c7297669bbb31e729e931da0d33f082ccd37
rheannemcintosh <rheannemcintosh@gmail.com>
2024-06-01 15:06:00 +0100

    Generate a maths lesson
    [GG-61]

---
+++ resources/views/components/maths-lesson.blade.php
@@ -0,0 +1,10 @@
+<div class="p-8">
+    <p class="mb-2 text-center">
+        Welcome to our Scottish Gaelic maths lesson. To complete this lesson, simply fill in the blanks to make the equations make sense! Good luck!
+    </p>
+    <livewire:maths :lesson="$lesson" />
+</div>
+
+
+
+
