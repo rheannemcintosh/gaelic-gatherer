6269ea993c096c771fedd970b2e63c880748d56b
rheannemcintosh <rheannemcintosh@gmail.com>
2024-06-03 22:59:30 +0100

    Update the lesson names and descriptions
    [GG-87]

--- database/seeders/LessonSeeder.php
+++ database/seeders/LessonSeeder.php
@@ -17,6 +17,7 @@ class LessonSeeder extends Seeder
             'unit_id' => 1,
             'lesson_type_id' => 1,
             'name' => 'Greetings Overview',
+            'description' => 'Learn the basics of how to greet people in Scottish Gaelic.',
             'required' => true,
         ]);
 
@@ -24,7 +25,7 @@ class LessonSeeder extends Seeder
             'unit_id' => 2,
             'lesson_type_id' => 1,
             'name' => 'Places Overview',
-            'description' => 'Learn how to greet people in Scottish Gaelic.',
+            'description' => 'Learn the basics of how to say where you\'re from.',
             'required' => true,
         ]);
 
@@ -32,6 +33,7 @@ class LessonSeeder extends Seeder
             'unit_id' => 3,
             'lesson_type_id' => 1,
             'name' => 'Food & Drink Overview',
+            'description' => 'Learn the basics of how to order food and drink.',
             'required' => true,
         ]);
 
@@ -39,6 +41,7 @@ class LessonSeeder extends Seeder
             'unit_id' => 4,
             'lesson_type_id' => 1,
             'name' => 'Weather Overview',
+            'description' => 'Learn the basics of how to talk about the weather.',
             'required' => true,
         ]);
 
@@ -46,47 +49,54 @@ class LessonSeeder extends Seeder
             'unit_id' => 5,
             'lesson_type_id' => 1,
             'name' => 'Numbers Overview',
+            'description' => 'Learn the basics of how to count from one to ten.',
             'required' => true,
         ]);
         DB::table('lessons')->insert([
             'unit_id' => 1,
             'lesson_type_id' => 5,
-            'name' => 'Match Greetings',
+            'name' => 'Matching Greetings',
+            'description' => 'Match Scottish Gaelic greetings with their English translation.',
             'required' => false,
         ]);
 
         DB::table('lessons')->insert([
             'unit_id' => 2,
             'lesson_type_id' => 5,
-            'name' => 'Match Places',
+            'name' => 'Matching Places',
+            'description' => 'Match Scottish Gaelic places with their English translation.',
             'required' => false,
         ]);
 
         DB::table('lessons')->insert([
             'unit_id' => 3,
             'lesson_type_id' => 5,
-            'name' => 'Match Food & Drink',
+            'name' => 'Matching Food & Drink',
+            'description' => 'Match Scottish Gaelic food and drink with their English translation.',
             'required' => false,
         ]);
 
         DB::table('lessons')->insert([
             'unit_id' => 4,
             'lesson_type_id' => 5,
-            'name' => 'Match Weather',
+            'name' => 'Matching Weather',
+            'description' => 'Match Scottish Gaelic weather phrases with their English translation.',
             'required' => false,
         ]);
 
         DB::table('lessons')->insert([
             'unit_id' => 5,
             'lesson_type_id' => 5,
-            'name' => 'Match Numbers',
+            'name' => 'Matching Numbers',
+            'description' => 'Match Scottish Gaelic numbers with their English translation.',
             'required' => false,
         ]);
 
         DB::table('lessons')->insert([
             'unit_id' => 5,
             'lesson_type_id' => 8,
-            'name' => 'Maths',
+            'name' => 'Calculations',
+            'description' => 'Complete the calculations to test your knowledge of numbers.',
             'required' => false,
         ]);
     }
