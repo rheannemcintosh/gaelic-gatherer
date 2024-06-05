e5675b79d0a1a71efadbfd268e24f3ad86ac29a1
rheannemcintosh <rheannemcintosh@gmail.com>
2024-06-05 21:58:48 +0100

    Add the gaelic gatherer logo to the navigation bar
    [GG-90]

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -6,7 +6,9 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('overview.show') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <div class="w-48">
+                            <img src="{{ asset('images/gaelic-gatherer-white-logo.png') }}">
+                        </div>
                     </a>
                 </div>
             </div>
