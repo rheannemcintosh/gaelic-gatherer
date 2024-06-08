a393980376b012fc607baf934efaa9aed8e53007
rheannemcintosh <rheannemcintosh@gmail.com>
2024-06-08 22:00:47 +0100

    Update the navigation bar
    [GG-6]

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,53 +1,33 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
-    <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-24">
-            <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('overview.show') }}">
-                        <div class="w-48">
-                            <img src="{{ asset('images/gaelic-gatherer-white-logo.png') }}">
-                        </div>
-                    </a>
-                </div>
-            </div>
-
-            <!-- Badges -->
-                @if (isset($badges) && !$hideButton)
-                    @if (auth()->user()->data->study_group == 'Experimental')
-                    <div class="z-10 mt-5" x-data="{ isVisible: false }">
-                        <div class="flex justify-center items-center z-10">
-                            <button @click="isVisible = !isVisible" class="justify-center bg-gradient-to-r from-blue-700 to-blue-500 px-8 py-4 rounded-lg shadow-md transition-all duration-200 ease-in-out transform hover:scale-105">
-                                <span class="text-white font-bold">View All Badges</span>
-                            </button>
-                        </div>
-                        <div  class="z-10 max-w-4xl mx-auto sm:px-6 lg:px-2 bg-white p-2">
-                            <div x-show="isVisible">
-                                <div class="flex justify-center items-center py-4 font-bold text-sm">Please hover over each badge to see how to earn it!</div>
-                                @foreach($badges->chunk(4) as $chunk)
-                                    <div class=" grid grid-cols-4">
-                                        @foreach($chunk as $badge)
-                                            <div class="m-1 p-2">
-                                                <livewire:badge-with-tooltip :badge="$badge" :key="$badge->id"/>
-                                            </div>
-                                        @endforeach
-                                    </div>
-                                @endforeach
-                            </div>
-                        </div>
+<nav class="bg-white relative p-4" x-data="{ isVisible: false, open: false }">
+    <div class="grid grid-cols-3 gap-4">
+        <!-- Logo -->
+        <div class="flex">
+            <div class="shrink-0 flex items-center">
+                <a href="{{ route('overview.show') }}">
+                    <div class="w-48">
+                        <img src="{{ asset('images/gaelic-gatherer-white-logo.png') }}">
                     </div>
-                @endif
-            @endif
-
+                </a>
+            </div>
+        </div>
+        <!-- View All Badges Button -->
+        <div class="flex justify-center">
+            <div class="flex justify-center items-center z-10">
+                <button @click="isVisible = !isVisible" class="justify-center bg-gradient-to-r from-blue-700 to-blue-500 px-8 py-4 rounded-lg shadow-md transition-all duration-200 ease-in-out transform hover:scale-105">
+                    <span class="text-white font-bold">View All Badges</span>
+                </button>
+            </div>
+        </div>
+        <!-- Dropdown Menu -->
+        <div class="flex justify-end ">
             <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
+            <div class="hidden sm:flex sm:items-center sm:ms-6 ">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-bold rounded-md text-white focus:outline-none transition ease-in-out duration-150 bg-blue-700 hover:bg-blue-900">
                             <div>{{ Auth::user()->name }}</div>
 
-                            <div class="ms-1">
+                            <div class="">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                                 </svg>
@@ -56,6 +36,9 @@
                     </x-slot>
 
                     <x-slot name="content">
+                        <x-dropdown-link :href="route('help.show')">
+                            Help
+                        </x-dropdown-link>
                         <x-dropdown-link :href="route('profile.edit')">
                             {{ __('Profile') }}
                         </x-dropdown-link>
@@ -71,48 +54,28 @@
                             </x-dropdown-link>
                         </form>
                     </x-slot>
-                </x-dropdown>
-            </div>
 
-            <!-- Hamburger -->
-            <div class="-me-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
+
+                </x-dropdown>
             </div>
         </div>
     </div>
 
-    <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
-            <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-            </div>
-
-            <div class="mt-3 space-y-1">
-                <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
-                </x-responsive-nav-link>
-
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <x-responsive-nav-link :href="route('logout')"
-                                           onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>
+    <!-- Badges Display -->
+    <div class="  flex justify-center">
+        <div class="absolute z-10 max-w-4xl mx-auto sm:px-6 lg:px-2 p-2 ">
+            <div class="bg-white rounded-md shadow-lg rounded-md ring-1 ring-black ring-opacity-5 p-8" x-show="isVisible">
+                <div class="flex justify-center items-center py-4 font-bold text-sm">Please hover over each badge to see how to earn it!</div>
+                @foreach($badges->chunk(4) as $chunk)
+                    <div class=" grid grid-cols-4">
+                        @foreach($chunk as $badge)
+                            <div class="m-1 p-2">
+                                <livewire:badge-with-tooltip :badge="$badge" :key="$badge->id"/>
+                            </div>
+                        @endforeach
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
 </nav>
-
