3409f70c37262e12b59832bbc06b5371342f2c3c
rheannemcintosh <rheannemcintosh@gmail.com>
2024-06-08 12:52:01 +0100

    Update login screen
    [GG-80]

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,6 @@
-<x-guest-layout>
+<x-app-layout>
+
+    <div class="p-8 bg-white shadow-md sm:rounded-lg">
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -40,4 +42,5 @@
             <button href="{{ route('register') }}" class="ml-2 inline-flex items-center px-4 py-2 bg-blue-700 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-900 focus:bg-blue-900 active:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Log In</button>
         </div>
     </form>
-</x-guest-layout>
+    </div>
+</x-app-layout>
