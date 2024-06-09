948571f24df3f6eebaf280fa7e541f4b97c2972d
rheannemcintosh <rheannemcintosh@gmail.com>
2024-06-09 18:31:41 +0100

    Remove forgot your password from the login screen
    [GG-110]

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,10 +40,6 @@
                 </div>
 
                 <div class="flex items-center justify-end mt-4">
-                    @if (Route::has('password.request'))
-                        <a href="{{ route('login') }}" class="mr-2 inline-flex items-center px-4 py-2 bg-white border border-blue-900 rounded-md font-semibold text-xs text-blue-900 uppercase tracking-widest shadow-sm hover:bg-blue-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150">Forgot Your Password?</a>
-                    @endif
-
                     <button href="{{ route('register') }}" class="ml-2 inline-flex items-center px-4 py-2 bg-blue-700 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-900 focus:bg-blue-900 active:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Log In</button>
                 </div>
             </form>
--- routes/auth.php
+++ routes/auth.php
@@ -22,9 +22,6 @@ Route::middleware('guest')->group(function () {
 
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
-    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
-                ->name('password.request');
-
     Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
                 ->name('password.email');
 
