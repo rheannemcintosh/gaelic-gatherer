f7cfb60bcc079d12b665fc1dabd21c1426033405
rheannemcintosh <rheannemcintosh@gmail.com>
2024-06-09 16:40:51 +0100

    Remove the pronunication from the numbers overview lesson
    [GG-100]

--- resources/views/components/numbers-overview.blade.php
+++ resources/views/components/numbers-overview.blade.php
@@ -8,7 +8,6 @@
                 <thead>
                 <tr>
                     <th class="px-4 py-2 text-center text-xl">Gaelic</th>
-                    <th class="px-4 py-2 text-center text-xl">Pronunciation</th>
                     <th class="px-4 py-2 text-center text-xl">Number</th>
                 </tr>
                 </thead>
@@ -16,53 +15,42 @@
                 <tr>
                     <td class="border px-4 py-2 text-center">One</td>
                     <td class="border px-4 py-2 text-center font-bold">Aon</td>
-                    <td class="border px-4 py-2 text-center italic">ah-uhn</td>
-
                 </tr>
                 <tr>
                     <td class="border px-4 py-2 text-center">Two</td>
                     <td class="border px-4 py-2 text-center font-bold">Dà</td>
-                    <td class="border px-4 py-2 text-center italic">dah</td>
                 </tr>
                 <tr>
                     <td class="border px-4 py-2 text-center">Three</td>
                     <td class="border px-4 py-2 text-center font-bold">Trì</td>
-                    <td class="border px-4 py-2 text-center italic">tree</td>
                 </tr>
                 <tr>
                     <td class="border px-4 py-2 text-center">Four</td>
                     <td class="border px-4 py-2 text-center font-bold">Ceithir</td>
-                    <td class="border px-4 py-2 text-center italic">keh-ir</td>
                 </tr>
                 <tr>
                     <td class="border px-4 py-2 text-center">Five</td>
                     <td class="border px-4 py-2 text-center font-bold">Còig</td>
-                    <td class="border px-4 py-2 text-center italic">ko-ik</td>
                 </tr>
                 <tr>
                     <td class="border px-4 py-2 text-center">Six</td>
                     <td class="border px-4 py-2 text-center font-bold">Sia</td>
-                    <td class="border px-4 py-2 text-center italic">shee-uh</td>
                 </tr>
                 <tr>
                     <td class="border px-4 py-2 text-center">Seven</td>
                     <td class="border px-4 py-2 text-center font-bold">Seachd</td>
-                    <td class="border px-4 py-2 text-center italic">shakht</td>
                 </tr>
                 <tr>
                     <td class="border px-4 py-2 text-center">Eight</td>
                     <td class="border px-4 py-2 text-center font-bold">Ochd</td>
-                    <td class="border px-4 py-2 text-center italic">ocht</td>
                 </tr>
                 <tr>
                     <td class="border px-4 py-2 text-center">Nine</td>
                     <td class="border px-4 py-2 text-center font-bold">Naoi</td>
-                    <td class="border px-4 py-2 text-center italic">nee</td>
                 </tr>
                 <tr>
                     <td class="border px-4 py-2 text-center">Ten</td>
                     <td class="border px-4 py-2 text-center font-bold">Deich</td>
-                    <td class="border px-4 py-2 text-center italic">jehkh</td>
                 </tr>
                 </tbody>
             </table>
