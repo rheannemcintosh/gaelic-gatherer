5790ef5eccbea057a8cc4a18f083f7d7050d2eaa
rheannemcintosh <rheannemcintosh@gmail.com>
2024-06-09 15:26:58 +0100

    Refactor the routes files
    [GG-100]

--- routes/frontend.php
+++ routes/frontend.php
@@ -1,19 +1,13 @@
 <?php
 
-use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
 
-    // Welcome Routes
-    Route::get('/', [FrontendController::class, 'displayWelcomePage'])->name('welcome');
+// Welcome Routes
+Route::get('/', [FrontendController::class, 'displayWelcomePage'])->name('welcome');
 
-    // Research Study Routes
-    Route::prefix('research-study')->group(function () {
-        Route::get('/', [FrontendController::class, 'displayResearchStudyInformation'])->name('research-study');
-        Route::get('/download-pdf', [FrontendController::class, 'downloadParticipantInformationSheetPDF'])->name('research-study.download-pdf');
-    });
-
-    Route::get('/privacy-policy', function () {
-        return view('pages.privacy-policy');
-    })->name('privacy-policy');
-
+// Research Study Routes
+Route::prefix('research-study')->group(function () {
+    Route::get('/', [FrontendController::class, 'displayResearchStudyInformation'])->name('research-study');
+    Route::get('/download-pdf', [FrontendController::class, 'downloadParticipantInformationSheetPDF'])->name('research-study.download-pdf');
+});
